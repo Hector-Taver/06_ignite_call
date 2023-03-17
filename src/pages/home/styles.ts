f0811dece7ea1f729d4fc017b9f097e083b90858
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-17 13:07:11 -0300

    created ClaimUsernameForm component

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,4 +1,4 @@
-import { Heading, styled } from '@ignite-ui/react'
+import { Heading, Text, styled } from '@ignite-ui/react'
 
 export const Container = styled('div', {
   maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
@@ -13,13 +13,13 @@ export const Hero = styled('div', {
   maxWidth: 480,
   padding: '0 $10',
 
-  [`${Heading}`]: {
+  [`> ${Heading}`]: {
     '@media(max-width: 600px)': {
       fontSize: '$6xl',
     },
   },
 
-  [`${Text}`]: {
+  [`> ${Text}`]: {
     marginTop: '$2',
     color: '$gray200',
   },
